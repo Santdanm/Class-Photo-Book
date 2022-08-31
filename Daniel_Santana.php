4d4f5f92ae6430afe7f94ebc9f3f8cd3e3eda89e
Santdanm <112573199+Santdanm@users.noreply.github.com>
2022-08-31 12:30:56 -0400

    Add files via upload

---
+++ Daniel_Santana.php
@@ -0,0 +1,99 @@
+<html lang="en">
+	<!-- https://www.bootdey.com/snippets/view/team-user-resume#html -->
+		<meta charset="utf-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+		<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous"><link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.12.1/css/all.min.css" integrity="sha256-mmgLkCYLUQbXn0B1SRqzHar6dCnv9oZFPEC1g1cwlkk=" crossorigin="anonymous" />
+
+		
+	<body>
+		<link rel="stylesheet" href="assets/css/detail.css" />
+		<title><?php echo 'ASE 230 - Daniel Santana'
+        ?></title>
+		<div class="container text-center mb-5">
+			<h1><?php echo 'This is ASE 230 - Daniel Santana'
+        ?></h1>
+        
+		</div>
+        <div class="container text-left mb-5">
+        <a href="Index.php"><button type="button" class="btn btn-primary btn-lg"><?php echo '<- Back' ?></button></a>
+        </div>
+		<div class="container">
+    <div class="row">
+      <div class="col-lg-5 col-md-6">
+        <div class="mb-2">
+          <img class="w-100" src="https://bootdey.com/img/Content/avatar/avatar7.png" alt="">
+        </div>
+        <div class="mb-2 d-flex">
+          <h4 class="font-weight-normal"><?php echo 'Daniel Santana'
+        ?></h4>
+          <div class="social d-flex ml-auto">
+            <p class="pr-2 font-weight-normal"><?php echo 'Follow on:'
+            ?></p>
+            <a href="https://www.facebook.com/profile.php?id=100010323518840" class="text-muted mr-1">
+              <i class="fab fa-facebook-f"></i>
+            </a>
+            <a href="#" class="text-muted mr-1">
+              <i class="fab fa-twitter"></i>
+            </a>
+            <a href="#" class="text-muted mr-1">
+              <i class="fab fa-instagram"></i>
+            </a>
+            <a href="https://www.linkedin.com/in/santanaadaniel/" class="text-muted">
+              <i class="fab fa-linkedin"></i>
+            </a>
+          </div>
+        </div>
+        <div class="mb-2">
+          <ul class="list-unstyled">
+            <li class="media">
+              <span class="w-45 text-black text font-weight-normal"><?php echo 'Dream profession: Cybersecurity Analyst'
+    ?></span>
+            </li>
+            <li class="media">
+              <span class="w-45 text-black font-weight-normal"><?php echo 'Dream company: Google' ?></span>
+            </li>
+            <li class="media">
+              <span class="w-45 text-black font-weight-normal"><?php echo 'Email: santanad2@nku.edu' ?></span>
+            </li>
+          </ul>
+        </div>
+      </div>
+      <div class="col-lg-7 col-md-6 pl-xl-3">
+        <h5 class="font-weight-normal"><?php echo 'About me' ?></h5>
+        <p><?php echo 'I am a senior and a current cybersecurity manjor. I am originally from New York and came here for college as it was cheap and allowed me to see my brother who lives out here. I am in a fraternity and have enjoyed my time here while making some great friends and connections.' ?></p>
+        <div class="my-2 bg-light p-2">
+          <p class="font-italic mb-0">The price is something not necessarily defined as financial. It could be time, effort, sacrifice, money or perhaps, something else.</p>
+        </div>
+        <div class="mb-2 mt-2 pt-1">
+          <h5 class="font-weight-normal">Top skills</h5>
+        </div>
+        <div class="py-1">
+          <div class="progress">
+            <div class="progress-bar" role="progressbr" style="width:85%" aria-valuenow="85" aria-valuemin="0" aria-valuemax="100">
+              <div class="progress-bar-title"><?php echo 'Programing: 85%'
+                ?></div>
+            
+            </div>
+          </div>
+        </div>
+        <div class="py-1">
+          <div class="progress">
+            <div class="progress-bar" role="progressbar" style="width:70%" aria-valuenow="70" aria-valuemin="0" aria-valuemax="100">
+              <div class="progress-bar-title"><?php echo 'Networking: 70%' ?></div>
+            </div>
+          </div>
+        </div>
+        <div class="py-1">
+          <div class="progress">
+            <div class="progress-bar" role="progressbar" style="width:77%" aria-valuenow="77" aria-valuemin="0" aria-valuemax="100">
+              <div class="progress-bar-title"><?php echo 'Education: 77%'?></div>
+            </div>
+          </div>
+        </div>
+        <h5 class="font-weight-normal">Fun fact</h5>
+        <p><?php echo 'I am a big baseball fan, my favorite team is the New York Mets and you can always find a pendent of their team logo on my necklace.' ?></p>
+      </div>
+    </div>
+  </div>
+	<script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+	<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ho+j7jyWK8fNQe+A12Hb8AhRq26LrZ/JpcUGGOn+Y7RsweNrtN/tE3MoK7ZeZDyx" crossorigin="anonymous"></script>
